9b8b5b7fe64db4c34cf854359cbcb8d5694db14b
zhong <313413092@qq.com>
2018-03-29 14:07:05 +0800

    添加商家管理展示所有功能 Add ng-repeat,brandController

--- pinyougou-manager-web/src/main/kotlin/com/pinyougou/manager/controller/BrandController.kt
+++ pinyougou-manager-web/src/main/kotlin/com/pinyougou/manager/controller/BrandController.kt
@@ -1,8 +1,8 @@
 package com.pinyougou.manager.controller
 
 import com.alibaba.dubbo.config.annotation.Reference
-import com.pinyougou.sellergoods.service.BrandService
 import com.pinyougou.pojo.TbBrand
+import com.pinyougou.sellergoods.service.BrandService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
