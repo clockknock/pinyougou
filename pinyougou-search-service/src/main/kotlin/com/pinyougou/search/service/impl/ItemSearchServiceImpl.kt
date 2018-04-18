37225a3cc7501b0a24882ebee058057f9eb610cb
zhong <313413092@qq.com>
2018-04-18 19:53:03 +0800

    增加 search模块的高亮查询 add search-service 普通查询改为高亮查询 add search-web angularApp添加trustAsHtml过滤器

--- pinyougou-search-service/src/main/kotlin/com/pinyougou/search/service/impl/ItemSearchServiceImpl.kt
+++ pinyougou-search-service/src/main/kotlin/com/pinyougou/search/service/impl/ItemSearchServiceImpl.kt
@@ -3,34 +3,56 @@ package com.pinyougou.search.service.impl
 import com.alibaba.dubbo.config.annotation.Service
 import com.pinyougou.pojo.TbItem
 import com.pinyougou.search.service.ItemSearchService
+import org.apache.commons.collections.CollectionUtils
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.solr.core.SolrTemplate
-import org.springframework.data.solr.core.query.Criteria
-import org.springframework.data.solr.core.query.Query
-import org.springframework.data.solr.core.query.SimpleQuery
+import org.springframework.data.solr.core.query.*
 
 /**
  * @author 钟未鸣
  * @date 2018/4/17
  */
 @Service(timeout = 5000)
-class ItemSearchServiceImpl : ItemSearchService{
+class ItemSearchServiceImpl : ItemSearchService {
     @Autowired
     private lateinit var solrTemplate: SolrTemplate
 
     override fun search(searchMap: MutableMap<String, Any>): MutableMap<String, Any> {
         //声明返回的map
-        val resultMap = mutableMapOf<String, Any>()
+        val resultMap = HashMap<String, Any>()
 
-        val query = SimpleQuery()
-        val criteria = Criteria("item_keywords").apply {
-            `is`(searchMap["keywords"])
-        }
-        query.addCriteria<Query>(criteria)
-        val scoredPage = solrTemplate.queryForPage(query, TbItem::class.java)
-        resultMap["rows"]=scoredPage.content
+        //关键字高亮查询
+        resultMap.putAll(highLightQuery(searchMap))
 
         return resultMap
+    }
+
+
+    private fun highLightQuery(searchMap: MutableMap<String, Any>): MutableMap<String, Any> {
+        val highlightQuery = SimpleHighlightQuery()
+        //1.初始化高亮选项
+        val options = HighlightOptions().apply {
+            addField("item_title")
+            simplePrefix = "<em style='color:red'>"
+            simplePostfix = "</em>"
+        }
+        highlightQuery.setHighlightOptions<HighlightQuery>(options)
+
+        highlightQuery.addCriteria<SimpleQuery>(Criteria("item_keywords").`is`
+        (searchMap["keywords"]))
+
+        val highlightPage = solrTemplate.queryForHighlightPage(highlightQuery, TbItem::class.java)
+        val highlighted = highlightPage.highlighted
+        highlighted.forEach { entry ->
+            //获得高亮列表
+            if (entry.highlights.isNotEmpty()) {
+                //将高亮内容放入contents
+                entry.entity.title = entry.highlights[0].snipplets[0]
+            }
+        }
+
+        return mutableMapOf(Pair("rows", highlightPage.content))
+
     }
 
 }
